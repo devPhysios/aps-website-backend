c3e51e5edc4ad77229702f858516690445e9bfae
Feranmi Oyebola <oyebolaferanmipeter@gmail.com>
2024-01-26 09:47:10 +0100

    Just observing

--- routes/profile.js
+++ routes/profile.js
@@ -1,9 +1,9 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
-const { updateStudent } = require('../controllers/profile')
+const { updateStudent } = require('../controllers/profile');
 
-router.route("/:id").patch(updateStudent)
+router.route("/:id").patch(updateStudent);
 // router.post('/login', login)
 
 module.exports = router
