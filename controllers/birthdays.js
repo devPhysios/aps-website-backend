cf788362b89fe72cf95b57947ee4c89263aa962a
FeranmiDev <macguzik98@gmail.com>
2024-06-29 15:58:13 +0100

    chore: Add routes for birthday events and delete functionality

--- controllers/birthdays.js
+++ controllers/birthdays.js
@@ -8,7 +8,8 @@ const capitalize = (word) => {
 const getStudentDetails = async (req, res) => {
   try {
     const { matricNumber: authMatricNumber } = req.student;
-    if (!authMatricNumber || authMatricNumber !== "213569") {
+    const allowedMatricNumbers = [213569, 220973];
+    if (!authMatricNumber || !allowedMatricNumbers.includes(authMatricNumber)) {
       return res
         .status(StatusCodes.UNAUTHORIZED)
         .json({ error: "You're not permitted to perform this action" });
@@ -46,23 +47,44 @@ const getStudentDetails = async (req, res) => {
 
 const createStudentBirthday = async (req, res) => {
   try {
-  const { matricNumber: authMatricNumber } = req.student;
-    if (!authMatricNumber || authMatricNumber !== "213569") {
+    const { matricNumber: authMatricNumber } = req.student;
+    const allowedMatricNumbers = [213569, 220973];
+    if (!authMatricNumber || !allowedMatricNumbers.includes(authMatricNumber)) {
       return res
         .status(StatusCodes.UNAUTHORIZED)
         .json({ error: "You're not permitted to perform this action" });
     }
 
-    const { matricNumber, fullName, birthdayMonth, birthdayDay, birthdayWish, level, classSet, imageUrl } = req.body;
+    const {
+      matricNumber,
+      fullName,
+      birthdayMonth,
+      birthdayDay,
+      birthdayWish,
+      level,
+      classSet,
+      imageUrl,
+    } = req.body;
 
-    if (!matricNumber || !fullName || !birthdayMonth || !birthdayDay || !birthdayWish || !level || !classSet || !imageUrl) {
+    if (
+      !matricNumber ||
+      !fullName ||
+      !birthdayMonth ||
+      !birthdayDay ||
+      !birthdayWish ||
+      !level ||
+      !classSet ||
+      !imageUrl
+    ) {
       return res.status(StatusCodes.BAD_REQUEST).json({ error: "Bad Request" });
     }
 
-    const student = await Student.findOne({ matricNumber})
+    const student = await Student.findOne({ matricNumber });
 
     if (!student) {
-      return res.status(StatusCodes.NOT_FOUND).json({ error: "Student Not Found" });
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ error: "Student Not Found" });
     }
 
     const birthday = await Birthday.create({
@@ -73,38 +95,33 @@ const createStudentBirthday = async (req, res) => {
       birthdayWish,
       level,
       classSet,
-      imageUrl
+      imageUrl,
     });
-    return res.status(StatusCodes.CREATED).json({ message: "Birthday created successfully", success: true, birthday});
+    return res
+      .status(StatusCodes.CREATED)
+      .json({
+        message: "Birthday created successfully",
+        success: true,
+        birthday,
+      });
   } catch (error) {
     console.log(error);
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: "Internal Server Error" });
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ error: "Internal Server Error" });
   }
-}
+};
 
 const updateStudentBirthday = async (req, res) => {
   try {
     const { matricNumber: authMatricNumber } = req.student;
-    console.log(authMatricNumber);
-    if (!authMatricNumber || authMatricNumber !== "213569") {
+    const allowedMatricNumbers = [213569, 220973];
+    if (!authMatricNumber || !allowedMatricNumbers.includes(authMatricNumber)) {
       return res
         .status(StatusCodes.UNAUTHORIZED)
         .json({ error: "You're not permitted to perform this action" });
     }
-
-    const { matricNumber, fullName, birthdayMonth, birthdayDay, birthdayWish, level, classSet, imageUrl } = req.body;
-
-    if (!matricNumber || !fullName || !birthdayMonth || !birthdayDay || !birthdayWish || !level || !classSet || !imageUrl) {
-      return res.status(StatusCodes.BAD_REQUEST).json({ error: "Bad Request" });
-    }
-
-    const student = await Student.findOne({ matricNumber})
-
-    if (!student) {
-      return res.status(StatusCodes.NOT_FOUND).json({ error: "Student Not Found" });
-    }
-
-    const birthday = await Birthday.findOneAndUpdate({ matricNumber }, {
+    const {
       matricNumber,
       fullName,
       birthdayMonth,
@@ -112,14 +129,58 @@ const updateStudentBirthday = async (req, res) => {
       birthdayWish,
       level,
       classSet,
-      imageUrl
-    }, { new: true });
-    return res.status(StatusCodes.OK).json({ message: "Birthday updated successfully", success: true, birthday});
+      imageUrl,
+    } = req.body;
+
+    if (
+      !matricNumber ||
+      !fullName ||
+      !birthdayMonth ||
+      !birthdayDay ||
+      !birthdayWish ||
+      !level ||
+      !classSet ||
+      !imageUrl
+    ) {
+      return res.status(StatusCodes.BAD_REQUEST).json({ error: "Bad Request" });
+    }
+
+    const student = await Student.findOne({ matricNumber });
+
+    if (!student) {
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ error: "Student Not Found" });
+    }
+
+    const birthday = await Birthday.findOneAndUpdate(
+      { matricNumber },
+      {
+        matricNumber,
+        fullName,
+        birthdayMonth,
+        birthdayDay,
+        birthdayWish,
+        level,
+        classSet,
+        imageUrl,
+      },
+      { new: true }
+    );
+    return res
+      .status(StatusCodes.OK)
+      .json({
+        message: "Birthday updated successfully",
+        success: true,
+        birthday,
+      });
   } catch (error) {
     console.log(error);
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: "Internal Server Error" });
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ error: "Internal Server Error" });
   }
-}
+};
 
 const getStudentBirthdays = async (req, res) => {
   try {
@@ -131,19 +192,97 @@ const getStudentBirthdays = async (req, res) => {
     // Find birthdays matching the current date
     const birthdays = await Birthday.find({
       birthdayMonth: currentMonth.toString(),
-      birthdayDay: currentDay.toString()
+      birthdayDay: currentDay.toString(),
     });
 
     if (birthdays.length === 0) {
-      return res.status(StatusCodes.OK).json({ birthdays: [], message: "No birthday events for today." });
+      return res
+        .status(StatusCodes.OK)
+        .json({ birthdays: [], message: "No birthday events for today." });
     }
 
     return res.status(StatusCodes.OK).json({ birthdays });
   } catch (error) {
     console.log(error);
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: "Internal Server Error" });
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ error: "Internal Server Error" });
   }
 };
 
+const getAllBirthdayEvents = async (req, res) => {
+  try {
+    const { matricNumber: authMatricNumber } = req.student;
+    if (!authMatricNumber || authMatricNumber !== "213569") {
+      return res
+        .status(StatusCodes.UNAUTHORIZED)
+        .json({ error: "You're not authorized to perform this action" });
+    }
 
-module.exports = { getStudentDetails, createStudentBirthday, updateStudentBirthday, getStudentBirthdays };
+    const birthdays = await Birthday.find();
+    if (birthdays.length === 0) {
+      return res
+        .status(StatusCodes.OK)
+        .json({ birthdays: [], message: "No birthday events available" });
+    }
+
+    return res.status(StatusCodes.OK).json({ birthdays });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ error: "Internal Server Error" });
+  }
+};
+
+const deleteStudentBirthday = async (req, res) => {
+  try {
+    const { matricNumber: authMatricNumber } = req.student;
+    const allowedMatricNumbers = ['213569', '220973'];
+    if (!authMatricNumber || !allowedMatricNumbers.includes(authMatricNumber)) {
+      return res
+        .status(StatusCodes.UNAUTHORIZED)
+        .json({ error: "You're not permitted to perform this action" });
+    }
+
+    const { matricNumber } = req.body;
+
+    if (!matricNumber) {
+      return res.status(StatusCodes.BAD_REQUEST).json({ error: "Bad Request" });
+    }
+
+    const student = await Student.findOne({ matricNumber });
+
+    if (!student) {
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ error: "Student Not Found" });
+    }
+
+    const birthday = await Birthday.findOneAndDelete({ matricNumber });
+
+    if (!birthday) {
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ error: "Birthday Not Found" });
+    }
+
+    return res
+      .status(StatusCodes.OK)
+      .json({ message: "Birthday deleted successfully", success: true });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ error: "Internal Server Error" });
+  }
+};
+
+module.exports = {
+  getStudentDetails,
+  createStudentBirthday,
+  updateStudentBirthday,
+  getStudentBirthdays,
+  getAllBirthdayEvents,
+  deleteStudentBirthday,
+};
--- routes/birthdays.js
+++ routes/birthdays.js
@@ -5,13 +5,17 @@ const {
   createStudentBirthday,
   updateStudentBirthday,
   getStudentBirthdays,
+  getAllBirthdayEvents,
+  deleteStudentBirthday
 } = require("../controllers/birthdays");
 const verifyToken = require("../middleware/authentication");
 // router.post('/register', register)
 
 router.get("/student/:studentMatricNumber", verifyToken, getStudentDetails);
+router.get("/allevents", verifyToken, getAllBirthdayEvents);
 router.get("/birthdayevents", getStudentBirthdays);
 router.post("/create", verifyToken, createStudentBirthday);
 router.patch("/update", verifyToken, updateStudentBirthday);
+router.delete("/delete", verifyToken, deleteStudentBirthday);
 
 module.exports = router;
